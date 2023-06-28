5a3c815cd4d0b86dd2afa15bab36aff293c82d26
SanChestery <121317406+SanChestery@users.noreply.github.com>
2023-06-28 14:33:27 +0200

    day 10 - calculator

---
+++ day 10/day-10-start.py
@@ -0,0 +1,9 @@
+
+# Function with Outputs
+def format_name(f_name, l_name):
+    f_new = f_name.title()
+    l_new = l_name.title()
+
+    return f"{f_new} {l_new}"
+
+print(format_name("YaNn", "sAnTsChI"))
