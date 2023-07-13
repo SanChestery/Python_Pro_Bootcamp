86e2c65521317a36539ef1db82114d7de846faa0
Yann Santschi <ysantschi@MacBook-Pro-von-Yann.local>
2023-07-12 20:43:40 -0700

    Day 22 - Pong Game

---
+++ Day 22/paddle.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+
+
+class Paddle(Turtle):
+
+    def __init__(self, x_cor):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(x_cor, 0)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
