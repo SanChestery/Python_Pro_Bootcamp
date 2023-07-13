1c5c5c60e0a8bf860d80187b6f2e1021310621c3
Yann Santschi <ysantschi@MacBook-Pro-von-Yann.local>
2023-07-13 09:58:54 -0700

    day 24 - added a high score written to a file

--- day 20/scoreboard.py
+++ day 20/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", mode="r") as file_highscore:
+            self.high_score = int(file_highscore.read())
         self.hideturtle()
         self.penup()
         self.goto(0, 260)
@@ -17,11 +18,14 @@ class Scoreboard(Turtle):
 
     def update(self):
         self.clear()
+        with open("data.txt", mode="r") as file_highscore:
+            self.high_score = int(file_highscore.read())
         self.write(arg=f"Score: {self.score} High Score {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
-            self.high_score = self.score
+            with open("data.txt", mode="w") as file_highscore:
+                file_highscore.write(str(self.score))
         self.score = 0
         self.update()
 
