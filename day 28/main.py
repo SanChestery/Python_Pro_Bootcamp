acfb8d5117870e429e2ff9eef2c953b1c28841ba
Yann Santschi <ysantschi@MacBook-Pro-von-Yann.local>
2023-07-20 12:46:12 -0700

    day 28 - Pomodoro Timer App

---
+++ day 28/main.py
@@ -0,0 +1,102 @@
+from tkinter import *
+import math
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_timer():
+    global timer, reps
+    window.after_cancel(timer)
+    reps = 0
+    label_timer.config(text="Timer", fg=GREEN)
+    cv_tomato.itemconfig(timer_text, text="00:00")
+    checkmark.config(text="")
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 2 != 0:
+        count_down(work_sec)
+        label_timer.config(text="Work", fg=GREEN)
+
+    elif reps % 2 == 0 and reps % 8 != 0:
+        count_down(short_break_sec)
+        label_timer.config(text="Break", fg=PINK)
+    else:
+        count_down(long_break_sec)
+        label_timer.config(text="Break", fg=RED)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+
+def count_down(count):
+    global reps, timer
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_min < 10:
+        count_min = f"0{count_min}"
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    cv_tomato.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            mark += "✓"
+        checkmark.config(text=mark)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+cv_tomato = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+cv_tomato.create_image(100, 112, image=tomato_img)
+timer_text = cv_tomato.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+label_timer = Label(text="TIMER", fg=GREEN, highlightthickness=0, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+label_timer.grid(column=1, row=0)
+
+button_start = Button(text="Start", highlightthickness=0, command=start_timer)
+button_start.grid(column=0, row=2)
+
+button_reset = Button(text="Reset", highlightthickness=0, command=reset_timer)
+button_reset.grid(column=2, row=2)
+
+checkmark = Label(fg=GREEN, bg=YELLOW, highlightthickness=0, font=(FONT_NAME, 20, "bold"))
+checkmark.grid(column=1, row=3)
+
+cv_tomato.grid(column=1, row=1)
+
+window.mainloop()
