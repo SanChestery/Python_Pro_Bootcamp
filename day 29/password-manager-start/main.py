8ff42de0006772c328e8750e0d1cdbb0628705c0
Yann Santschi <ysantschi@MacBook-Pro-von-Yann.local>
2023-07-29 22:07:50 -0700

    day 30 - improvements with JSON and error handling

--- day 29/password-manager-start/main.py
+++ day 29/password-manager-start/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def gen_pw():
@@ -27,22 +28,49 @@ def gen_pw():
 
 def password_add():
 
-    website = entry_website.get()
+    website = entry_website.get().lower()
     uname = entry_uname.get()
     pw = entry_pw.get()
+    new_data = {
+        website: {
+            "email": uname,
+            "password": pw
+        }
+    }
 
     if len(website) < 1 or len(uname) < 1 or len(pw) < 1:
         messagebox.askquestion(title="Oops", message="Please don't leave any fields emtpy!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"Entered Details: \nEmail: {uname}\nPassword: {pw}\nSave?")
+            try:
+                with open("data.json", 'r') as data_file:
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", 'w') as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                data.update(new_data)
+                with open("data.json", 'w') as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
+                entry_website.delete(0, END)
+                entry_pw.delete(0, END)
 
-        if is_ok:
-            with open("passwords.txt", 'a') as passfile:
-                passfile.write(f"\n{website} | {uname} | {pw}")
-            entry_website.delete(0, END)
-            entry_pw.delete(0, END)
+
+def find_password():
+    website = entry_website.get().lower()
+    try:
+        with open("data.json", 'r') as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="File not found", message="No entries available!")
+    else:
+        if website in data:
+            messagebox.showinfo(title=website, message=f"Email: {data[website]['email']}\nPassword: {data[website]['password']}")
+        else:
+            messagebox.showerror(title=website, message=f"No details found")
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Password Manager")
@@ -55,13 +83,13 @@ cv.grid(row=0, column=1)
 
 label_website = Label(text="Website:")
 label_website.grid(row=1, column=0)
-entry_website = Entry(width=35)
+entry_website = Entry(width=18)
 entry_website.focus()
-entry_website.grid(row=1, column=1, columnspan=2)
+entry_website.grid(row=1, column=1)
 
 label_uname = Label(text="Email/Username:")
 label_uname.grid(row=2, column=0)
-entry_uname = Entry(width=35)
+entry_uname = Entry(width=42)
 entry_uname.insert(0, "dummy@email.com")
 entry_uname.grid(row=2, column=1, columnspan=2)
 
@@ -69,13 +97,12 @@ label_pw = Label(text="Password:")
 label_pw.grid(row=3, column=0)
 entry_pw = Entry(width=18)
 entry_pw.grid(row=3, column=1)
-button_pw = Button(text="Generate Password", command=gen_pw)
+button_pw = Button(text="Generate Password", width=18, command=gen_pw)
 button_pw.grid(row=3, column=2)
 
-button_add = Button(text="Add", width=36, command=password_add)
+button_add = Button(text="Add", width=42, command=password_add)
 button_add.grid(row=4, column=1, columnspan=2)
 
-
-
-
+button_search = Button(text="Search", width=18, command=find_password)
+button_search.grid(row=1, column=2)
 window.mainloop()
